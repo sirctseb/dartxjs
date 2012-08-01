b943415345929930de8213550307f545d5ddf04b
Christopher Best <bestchris@gmail.com>
2012-08-01 10:43:16 -0400

    changed semantics
    made dart side all static
    made receiveMessage a one-off operation
    added addReceiver for continuing receipt of messages

--- dartxjs.dart
+++ dartxjs.dart
@@ -3,31 +3,66 @@
 #import('dart:json');
 
 class DartToJSCommunicator {
-  Function _handler;
-  
-  DartToJSCommunicator() {
-    _handler = null;
-    window.on.message.add((MessageEvent e) {
-      if (_handler == null)
+  static List<Function> _handlers;
+  static List<Function> _onceHandlers;
+
+  static void initialize() {
+    if(_handlers == null) {
+      _handlers = new List<Function>();
+      _onceHandlers = new List<Function>();
+
+      window.on.message.add(_dispatchMessage);
+    }
+  }
+
+  static void _dispatchMessage(event) {
+    if(_handlers.length == 0 && _onceHandlers.length == 0) {
+      return;
+    }
+    try {
+      String json_data = event.data;
+      Map<String, Object> cmd = JSON.parse(json_data);
+      if(!(cmd is Map) || cmp["rcpt"] != "dart") {
         return;
-      try {
-        String json_data = e.data;
-        Map<String,Object> cmd = JSON.parse(json_data);
-        if (!(cmd is Map) || cmd["rcpt"] != "dart") {
-          return;
-        }
-        _handler(cmd["data"]);
-      } catch (Exception ex) {
-        // ignore data that can't be parsed
       }
-    });
+
+      // call receiver functions
+      for(Function handler in _handlers) {
+        handler(cmd["data"]);
+      }
+
+      // call once handlers
+      for(Function handler in _onceHandlers) {
+        handler(cmd["data"]);
+      }
+      // clear once handlers
+      _onceHandlers.clear();
+
+    } catch (Exception e) {
+      // ignore data that can't be parsed
+    }
   }
-  
-  void set receiveMessage(Function f) {
-    _handler = f;
+
+  // send a message to js
+  static void sendMessage(Map<String, Object> data) {
+    window.postMessage(JSON.stringify({"rcpt": "js", "data": data}), window.location.href);
   }
-  
-  void sendMessage(Map<String,Object> data) {
-    window.postMessage(JSON.stringify({"rcpt":"js", "data":data}), window.location.href);
+
+  // add a function to be called when dart gets a message
+  static void addReceiver(Function f) {
+    _handler.add(f);
+  }
+  // remove a funciton as a receiver
+  static void removeReceiver(Function f) {
+    int index = _handlers.indexOf(f);
+    if(index != -1) {
+      _handlers.removeRange(index, 1);
+    }
+  }
+
+  // add a function to be called once when dart gets a message
+  static void receiveMessage(Function f) {
+    // add handler
+    _onceHandlers.add(f);
   }
 }
--- dartxjs.js
+++ dartxjs.js
@@ -1,31 +1,65 @@
 JSToDartCommunicator = (function() {
 
-	var addEventListener = function(elem, event, f) {
-		if (elem.addEventListener) {
-			elem.addEventListener(event, f, false);
-		} else if (elem.attachEvent) {
-			elem.attachEvent('on' + event, f);
-		} else {
-			throw "couldn't attach event " + event + " to " + elem;
+	var that = {};
+
+	var handlers = [];
+	var onceHandlers = [];
+
+	// handle messages to check if they are for js,
+	// and dispatch to handlers
+	var dispatchMessage = function(event) {
+		try {
+			var pkt = JSON.parse(event.data);
+			if(pkt.rcpt === "js") {
+				// call handlers
+				for(var handler in handlers) {
+					handlers[handler](pkt.data);
+				}
+				// call once handlers
+				for(var handler in onceHandlers) {
+					onceHandlers[handlers](pkt.data);
+				}
+				// clear once handlers
+				onceHandlers = [];
+			}
+		} catch (e) {
+			// ignore exceptions
 		}
 	};
 
+	// register for window message event
+	if(window.addEventListener) {
+		window.addEventListener.("message", dispatchMessage, false);
+	} else if(window.attachEvent) {
+		window.attachEvent("onmessage", dispatchMessage);
+	} else {
+		throw "Couldn't attach message event handler"
+	}
+
 	return {
+		// send a message to dart
 		sendMessage: function(data) {
 			window.postMessage(JSON.stringify({"rcpt":"dart", "data":data}), window.location.href);
 		},
 
+		// add a function to be called when js gets a message
+		addReceiver: function(f) {
+			// add handler
+			handlers.push(f);
+		}
+		// remove a function as a receiver
+		removeReceiver: function(f) {
+			// remove the function
+			var index = handlers.indexOf(f);
+			if(index !== -1) {
+				handlers.splice(index, 1);
+			}
+		}
+
+		// add a function to be called once when js gets a message
 		receiveMessage: function(f) {
-			addEventListener(window, 'message', function(event) {
-				try {
-					var pkt = JSON.parse(event.data);
-					if (pkt.rcpt == "js") {
-						f(pkt["data"]);
-					}
-				} catch (e) {
-					// ignore exceptions
-				}
-			});
+			// add handler
+			onceHandlers.push(f);
 		}
 	}
 })();
